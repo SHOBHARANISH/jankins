9ad582954d34f12e24fbe94de80529db4b821967
SHOBHARANISH <shobhash2426@gmail.com>
2025-08-29 10:32:54 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
 public class Test{
-  public static void main(String[]ars)
-  System.out.println("hellow world")
-{
+  public static void main(String[]args){
+  System.out.println("hellow world");
+  }
 }
